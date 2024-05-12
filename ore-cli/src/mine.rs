488065d4950f57bfa6ac79a46fad313b5af6afd6
kongtouhunter <165746355+kongtouhunter@users.noreply.github.com>
2024-05-12 12:56:03 +0800

    Add files via upload

---
+++ ore-cli/src/mine.rs
@@ -0,0 +1,209 @@
+use std::{sync::Arc, time::Instant};
+
+use ore::{self, state::Proof, BUS_ADDRESSES, BUS_COUNT, EPOCH_DURATION};
+use rand::Rng;
+use solana_program::pubkey::Pubkey;
+use solana_rpc_client::spinner;
+use solana_sdk::signer::Signer;
+
+#[cfg(feature = "gpu")]
+use drillx::{
+    gpu::{drill_hash, gpu_init, set_noise},
+    noise::NOISE,
+};
+
+use crate::{
+    send_and_confirm::ComputeBudget,
+    utils::{amount_u64_to_string, get_clock, get_config, get_proof},
+    Miner,
+};
+
+impl Miner {
+    pub async fn mine(&self, threads: u64, buffer_time: u64) {
+        // Register, if needed.
+        let signer = self.signer();
+        self.register().await;
+
+        loop {
+            // Fetch proof
+            let proof = get_proof(&self.rpc_client, signer.pubkey()).await;
+            println!(
+                "\nStake balance: {} ORE",
+                amount_u64_to_string(proof.balance)
+            );
+
+            // Calc cutoff time
+            let cutoff_time = self.get_cutoff(proof, buffer_time).await;
+
+            // Run drillx (gpu)
+            #[cfg(feature = "gpu")]
+            let nonce = self.find_hash_gpu(proof, cutoff_time).await;
+
+            // Run drillx
+            #[cfg(not(feature = "gpu"))]
+            let nonce = self.find_hash_par(proof, cutoff_time, threads).await;
+
+            // Submit most difficult hash
+            let mut ixs = vec![];
+            if self.needs_reset().await {
+                ixs.push(ore::instruction::reset(signer.pubkey()));
+            }
+            ixs.push(ore::instruction::mine(signer.pubkey(), find_bus(), nonce));
+            self.send_and_confirm(&ixs, ComputeBudget::Fixed(250_000), false)
+                .await
+                .ok();
+        }
+    }
+
+    #[cfg(feature = "gpu")]
+    async fn find_hash_gpu(&self, proof: Proof, cutoff_time: u64) -> u64 {
+        let progress_bar = Arc::new(spinner::new_progress_bar());
+        progress_bar.set_message("Mining (gpu)...");
+
+        // Initial gpu memory
+        unsafe {
+            gpu_init();
+            set_noise(NOISE.as_usize_slice().as_ptr());
+        }
+
+        // Hash on gpu
+        let challenge = proof.challenge;
+        let mut gpu_nonce = [0; 8];
+        unsafe {
+            drill_hash(
+                challenge.as_ptr(),
+                gpu_nonce.as_mut_ptr(),
+                cutoff_time.max(5).min(60),
+            );
+        }
+
+        // Calculate hash and difficulty
+        let hx = drillx::hash(&challenge, &gpu_nonce);
+        let difficulty = drillx::difficulty(hx);
+        progress_bar.finish_with_message(format!(
+            "Best hash: {} (difficulty: {})",
+            bs58::encode(hx).into_string(),
+            difficulty
+        ));
+
+        // Return nonce
+        u64::from_le_bytes(gpu_nonce)
+    }
+
+    async fn find_hash_par(&self, proof: Proof, cutoff_time: u64, threads: u64) -> u64 {
+        // Check num threads
+        self.check_num_cores(threads);
+
+        // Dispatch job to each thread
+        let progress_bar = Arc::new(spinner::new_progress_bar());
+        progress_bar.set_message("Mining...");
+        let handles: Vec<_> = (0..threads)
+            .map(|i| {
+                std::thread::spawn({
+                    let proof = proof.clone();
+                    let progress_bar = progress_bar.clone();
+                    move || {
+                        let timer = Instant::now();
+                        let first_nonce = u64::MAX.saturating_div(threads).saturating_mul(i);
+                        let mut nonce = first_nonce;
+                        let mut best_nonce = nonce;
+                        let mut best_difficulty = 0;
+                        let mut best_hash = [0; 32];
+                        loop {
+                            // Create hash
+                            let hx = drillx::hash(&proof.challenge, &nonce.to_le_bytes());
+                            let difficulty = drillx::difficulty(hx);
+
+                            // Check difficulty
+                            if difficulty.gt(&best_difficulty) {
+                                best_nonce = nonce;
+                                best_difficulty = difficulty;
+                                best_hash = hx;
+                            }
+
+                            // Exit if time has elapsed
+                            if nonce % 10_000 == 0 {
+                                if timer.elapsed().as_secs().ge(&cutoff_time) {
+                                    if best_difficulty.gt(&ore::MIN_DIFFICULTY) {
+                                        // Mine until min difficulty has been met
+                                        break;
+                                    }
+                                } else if i == 0 {
+                                    progress_bar.set_message(format!(
+                                        "Mining... ({} sec remaining)",
+                                        cutoff_time.saturating_sub(timer.elapsed().as_secs()),
+                                    ));
+                                }
+                            }
+
+                            // Increment nonce
+                            nonce += 1;
+                        }
+
+                        // Return the best nonce
+                        (best_nonce, best_difficulty, best_hash)
+                    }
+                })
+            })
+            .collect();
+
+        // Join handles and return best nonce
+        let mut best_nonce = 0;
+        let mut best_difficulty = 0;
+        let mut best_hash = [0; 32];
+        for h in handles {
+            if let Ok((nonce, difficulty, hash)) = h.join() {
+                if difficulty > best_difficulty {
+                    best_difficulty = difficulty;
+                    best_nonce = nonce;
+                    best_hash = hash;
+                }
+            }
+        }
+
+        // Update log
+        progress_bar.finish_with_message(format!(
+            "Best hash: {} (difficulty: {})",
+            bs58::encode(best_hash).into_string(),
+            best_difficulty
+        ));
+
+        best_nonce
+    }
+
+    pub fn check_num_cores(&self, threads: u64) {
+        // Check num threads
+        let num_cores = num_cpus::get() as u64;
+        if threads.gt(&num_cores) {
+            println!(
+                "WARNING: Number of threads ({}) exceeds available cores ({})",
+                threads, num_cores
+            );
+        }
+    }
+
+    async fn needs_reset(&self) -> bool {
+        let clock = get_clock(&self.rpc_client).await;
+        let config = get_config(&self.rpc_client).await;
+        config
+            .last_reset_at
+            .saturating_add(EPOCH_DURATION)
+            .le(&clock.unix_timestamp)
+    }
+
+    async fn get_cutoff(&self, proof: Proof, buffer_time: u64) -> u64 {
+        let clock = get_clock(&self.rpc_client).await;
+        proof
+            .last_hash_at
+            .saturating_add(60)
+            .saturating_sub(buffer_time as i64)
+            .saturating_sub(clock.unix_timestamp)
+            .max(0) as u64
+    }
+}
+
+// TODO Pick a better strategy (avoid draining bus)
+fn find_bus() -> Pubkey {
+    let i = rand::thread_rng().gen_range(0..BUS_COUNT);
+    BUS_ADDRESSES[i]
+}
---
+++ ore-cli/src/utils.rs
@@ -0,0 +1,66 @@
+use cached::proc_macro::cached;
+use ore::{
+    self,
+    state::{Config, Proof, Treasury},
+    utils::AccountDeserialize,
+    CONFIG_ADDRESS, MINT_ADDRESS, PROOF, TREASURY_ADDRESS,
+};
+use solana_client::nonblocking::rpc_client::RpcClient;
+use solana_program::{pubkey::Pubkey, sysvar};
+use solana_sdk::clock::Clock;
+use spl_associated_token_account::get_associated_token_address;
+
+pub async fn _get_treasury(client: &RpcClient) -> Treasury {
+    let data = client
+        .get_account_data(&TREASURY_ADDRESS)
+        .await
+        .expect("Failed to get treasury account");
+    *Treasury::try_from_bytes(&data).expect("Failed to parse treasury account")
+}
+
+pub async fn get_config(client: &RpcClient) -> Config {
+    let data = client
+        .get_account_data(&CONFIG_ADDRESS)
+        .await
+        .expect("Failed to get config account");
+    *Config::try_from_bytes(&data).expect("Failed to parse config account")
+}
+
+pub async fn get_proof(client: &RpcClient, authority: Pubkey) -> Proof {
+    let proof_address = proof_pubkey(authority);
+    let data = client
+        .get_account_data(&proof_address)
+        .await
+        .expect("Failed to get miner account");
+    *Proof::try_from_bytes(&data).expect("Failed to parse miner account")
+}
+
+pub async fn get_clock(client: &RpcClient) -> Clock {
+    let data = client
+        .get_account_data(&sysvar::clock::ID)
+        .await
+        .expect("Failed to get miner account");
+    bincode::deserialize::<Clock>(&data).expect("Failed to deserialize clock")
+}
+
+pub fn amount_u64_to_string(amount: u64) -> String {
+    amount_u64_to_f64(amount).to_string()
+}
+
+pub fn amount_u64_to_f64(amount: u64) -> f64 {
+    (amount as f64) / 10f64.powf(ore::TOKEN_DECIMALS as f64)
+}
+
+pub fn amount_f64_to_u64(amount: f64) -> u64 {
+    (amount * 10f64.powf(ore::TOKEN_DECIMALS as f64)) as u64
+}
+
+#[cached]
+pub fn proof_pubkey(authority: Pubkey) -> Pubkey {
+    Pubkey::find_program_address(&[PROOF, authority.as_ref()], &ore::ID).0
+}
+
+#[cached]
+pub fn treasury_tokens_pubkey() -> Pubkey {
+    get_associated_token_address(&TREASURY_ADDRESS, &MINT_ADDRESS)
+}
